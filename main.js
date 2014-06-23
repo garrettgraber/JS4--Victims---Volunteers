08ec2207a4a0cc322575ad87319f583728e21f73
garrettgraber <garrett_graber@yahoo.com>
2014-06-23 17:23:42 -0600

    JS4 done, Bonus I, Bonus II also done with multiple volunteers per street

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 
+//Main part of exercise
+
+
+
 var numberDisasterVictims = window.prompt("How many disaster victims do you wish to enter: ", "");
 
 var nameArray = [];
@@ -36,5 +40,70 @@ var peopleInNeed = "People in Need: " + numberDisasterVictims + '\n';
 var peopleVolunteers = "Number of volunteers: " + numberVolunteers + '\n';
 var peopleInNeedNames = "Disaster victims: " + nameArray.join(', ') + '\n';
 var peopleVolunteersNames = "Volunteers: " + nameArrayVolunteers.join(', ') + '\n';
+var totalOutput = peopleInNeed + peopleVolunteers + peopleInNeedNames + peopleVolunteersNames;
 
-alert(peopleInNeed + peopleVolunteers + peopleInNeedNames + peopleVolunteersNames);
+alert(totalOutput);
+console.log(totalOutput);
+
+
+
+//Bonus I.
+
+
+
+var addNames = true;
+
+while (addNames === true) {
+
+	var nameInput = window.prompt("Name: ", "");
+	var phoneInput = window.prompt("Phone number: ", "");
+	var streetInput = window.prompt("Street: ", "");
+
+	nameArray.push(nameInput);
+	phoneArray.push(phoneInput);
+	streetArray.push(streetInput);
+
+	var confirmInput = window.confirm("Add another victim?");
+
+	if (confirmInput === false) {
+		addNames = false;
+	}
+
+}
+
+var peopleInNeedNames = "Disaster victims: " + nameArray.join(', ') + '\n';
+alert(peopleInNeedNames);
+
+
+
+
+//Bonus II.
+
+var PersonInNeed = window.prompt("Enter a person in need: ", "");
+var PersonInNeedIndex = nameArray.indexOf(PersonInNeed);
+var StreetPersonInNeed = streetArray[PersonInNeedIndex];
+var volunteerSameStreetArray = [];
+
+for ( var startSearch = 0; startSearch < streetArrayVolunteers.length; startSearch++) {
+	var VolunteerIndex = streetArrayVolunteers.indexOf(StreetPersonInNeed, startSearch);
+	console.log(VolunteerIndex);
+
+	if (VolunteerIndex > -1) {
+		var VolunteerName = nameArrayVolunteers[VolunteerIndex];
+		var VolunteerStreet = streetArrayVolunteers[VolunteerIndex];
+		volunteerSameStreetArray.push(VolunteerName);
+	}
+	else {
+		alert("No volunteers on the same street");
+	}
+}
+
+var volunteersOnSameStreet = "Volunteers on the same street: " + volunteerSameStreetArray.join(', ') + '\n';
+alert(volunteersOnSameStreet);
+
+
+
+
+
+
+
